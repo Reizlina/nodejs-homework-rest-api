0aedbb6df49ba1bf1270a7909a326c991ff55b1d
Reizlina Tanya <97384405+Reizlina@users.noreply.github.com>
2022-12-09 14:51:01 +0200

    Hope it's works)

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -8,7 +8,7 @@ const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
-    throw createError(401, "Email or password invalid");
+    throw createError(401, "Email or password is wrong");
   }
 
   const comparePassword = await bcrypt.compare(password, user.password);
@@ -22,8 +22,15 @@ const login = async (req, res) => {
 
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
   await User.findByIdAndUpdate(user._id, { token });
+
+  const aboutUser = {
+    email: user.email,
+    subscription: user.subscription,
+  };
+
   res.json({
     token,
+    aboutUser,
   });
 };
 
